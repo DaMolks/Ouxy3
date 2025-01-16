419c6035044b62d9f3faa75a0e92d01c6c6de713
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-16 21:59:59 +0100

    feat: Update ViewModelModule with SitesSetupViewModel

--- app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
+++ app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
@@ -1,6 +1,7 @@
 package com.damolks.ouxy3.di
 
 import com.damolks.ouxy3.ui.onboarding.signature.SignatureViewModel
+import com.damolks.ouxy3.ui.onboarding.sites.SitesSetupViewModel
 import com.damolks.ouxy3.ui.onboarding.technician.TechnicianProfileViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModel { TechnicianProfileViewModel(get()) }
     viewModel { SignatureViewModel(get()) }
+    viewModel { SitesSetupViewModel(get()) }
 }
