d2eaa6aceb5f5a78e447ec3ea0a306347c1284d3
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-16 21:57:35 +0100

    feat: Update ViewModelModule with SignatureViewModel

--- app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
+++ app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.damolks.ouxy3.di
 
+import com.damolks.ouxy3.ui.onboarding.signature.SignatureViewModel
 import com.damolks.ouxy3.ui.onboarding.technician.TechnicianProfileViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { TechnicianProfileViewModel(get()) }
+    viewModel { SignatureViewModel(get()) }
 }
