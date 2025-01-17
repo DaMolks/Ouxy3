b83f5972c291b159704772d39d18b5508e130751
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-17 17:49:51 +0100

    feat: Update ViewModelModule with HomeViewModel

--- app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
+++ app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.damolks.ouxy3.di
 
+import com.damolks.ouxy3.ui.main.home.HomeViewModel
 import com.damolks.ouxy3.ui.onboarding.signature.SignatureViewModel
 import com.damolks.ouxy3.ui.onboarding.sites.SitesSetupViewModel
 import com.damolks.ouxy3.ui.onboarding.technician.TechnicianProfileViewModel
@@ -7,7 +8,11 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
+    // Onboarding ViewModels
     viewModel { TechnicianProfileViewModel(get()) }
     viewModel { SignatureViewModel(get()) }
     viewModel { SitesSetupViewModel(get()) }
+
+    // Main ViewModels
+    viewModel { HomeViewModel(get(), get()) }
 }
