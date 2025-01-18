43eb2ef6d8e68df3c00fa28ffdbaff241bfabb72
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-18 21:49:42 +0100

    feat: Ajout du TechnicianProfileViewModel dans le module Koin

--- app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
+++ app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
@@ -2,10 +2,12 @@ package com.damolks.ouxy3.di
 
 import com.damolks.ouxy3.ui.main.marketplace.MarketplaceViewModel
 import com.damolks.ouxy3.ui.main.settings.SettingsViewModel
+import com.damolks.ouxy3.ui.onboarding.technician.TechnicianProfileViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { MarketplaceViewModel(get()) }
     viewModel { SettingsViewModel(get()) }
+    viewModel { TechnicianProfileViewModel(get()) }
 }
