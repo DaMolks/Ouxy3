dc03b01c53b7dd92a56f406e11598446d892550c
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-18 21:46:40 +0100

    feat: Ajout UserPreferencesRepository dans le module

--- app/src/main/java/com/damolks/ouxy3/di/RepositoryModule.kt
+++ app/src/main/java/com/damolks/ouxy3/di/RepositoryModule.kt
@@ -1,8 +1,12 @@
 package com.damolks.ouxy3.di
 
 import com.damolks.ouxy3.data.repository.MarketplaceRepository
+import com.damolks.ouxy3.data.repository.UserPreferencesRepository
+import com.damolks.ouxy3.data.repository.UserPreferencesRepositoryImpl
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single { MarketplaceRepository(get()) }
+    single<UserPreferencesRepository> { UserPreferencesRepositoryImpl(androidContext()) }
 }
