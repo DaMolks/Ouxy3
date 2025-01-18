c0e0be81ee98c1c9c7ff9d51cecea0c12250e42a
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-18 21:50:24 +0100

    feat: Ajout du modèle TechnicianFormState

---
+++ app/src/main/java/com/damolks/ouxy3/ui/onboarding/technician/TechnicianFormState.kt
@@ -0,0 +1,9 @@
+package com.damolks.ouxy3.ui.onboarding.technician
+
+data class TechnicianFormState(
+    val firstName: String,
+    val lastName: String,
+    val matricule: String,
+    val sector: String,
+    val teamLeader: String
+)
