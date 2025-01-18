399464d586abc8389a372c65a157411bdbd17999
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-18 12:44:58 +0100

    feat: Add CRUD operations to Sites screen

--- app/src/main/java/com/damolks/ouxy3/ui/main/sites/SitesFragment.kt
+++ app/src/main/java/com/damolks/ouxy3/ui/main/sites/SitesFragment.kt
@@ -4,19 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.damolks.ouxy3.data.repository.SiteRepository
+import com.damolks.ouxy3.data.model.Site
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.damolks.ouxy3.databinding.FragmentSitesBinding
+import com.damolks.ouxy3.databinding.DialogAddSiteBinding
 import kotlinx.coroutines.launch
-import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SitesFragment : Fragment() {
 
     private var _binding: FragmentSitesBinding? = null
     private val binding get() = _binding!!
-    private val siteRepository: SiteRepository by inject()
+    private val viewModel: SitesViewModel by viewModel()
     private lateinit var sitesAdapter: SitesAdapter
 
     override fun onCreateView(
@@ -31,7 +34,7 @@ class SitesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupUI()
-        loadSites()
+        observeViewModel()
     }
 
     override fun onDestroyView() {
@@ -40,22 +43,108 @@ class SitesFragment : Fragment() {
     }
 
     private fun setupUI() {
-        sitesAdapter = SitesAdapter()
+        sitesAdapter = SitesAdapter(
+            onSiteClick = { site ->
+                showEditSiteDialog(site)
+            },
+            onDeleteClick = { site ->
+                showDeleteConfirmation(site)
+            }
+        )
+
         binding.sitesRecyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = sitesAdapter
         }
 
         binding.fabAddSite.setOnClickListener {
-            // TODO: Implémenter l'ajout de site
+            showAddSiteDialog()
         }
     }
 
-    private fun loadSites() {
+    private fun observeViewModel() {
         viewLifecycleOwner.lifecycleScope.launch {
-            siteRepository.getAllSites().collect { sites ->
+            viewModel.sites.collect { sites ->
                 sitesAdapter.submitList(sites)
             }
         }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.events.collect { event ->
+                when (event) {
+                    is SiteEvent.Success -> Toast.makeText(requireContext(), event.message, Toast.LENGTH_SHORT).show()
+                    is SiteEvent.Error -> Toast.makeText(requireContext(), event.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun showAddSiteDialog() {
+        val dialogBinding = DialogAddSiteBinding.inflate(layoutInflater)
+
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Ajouter un site")
+            .setView(dialogBinding.root)
+            .setPositiveButton("Ajouter") { dialog, _ ->
+                val site = Site(
+                    name = dialogBinding.siteName.text.toString(),
+                    address = dialogBinding.siteAddress.text.toString(),
+                    contactName = dialogBinding.contactName.text.toString(),
+                    contactPhone = dialogBinding.contactPhone.text.toString(),
+                    notes = dialogBinding.notes.text.toString()
+                )
+                viewModel.addSite(site)
+                dialog.dismiss()
+            }
+            .setNegativeButton("Annuler") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun showEditSiteDialog(site: Site) {
+        val dialogBinding = DialogAddSiteBinding.inflate(layoutInflater)
+
+        // Pré-remplir les champs
+        with(dialogBinding) {
+            siteName.setText(site.name)
+            siteAddress.setText(site.address)
+            contactName.setText(site.contactName)
+            contactPhone.setText(site.contactPhone)
+            notes.setText(site.notes)
+        }
+
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Modifier le site")
+            .setView(dialogBinding.root)
+            .setPositiveButton("Modifier") { dialog, _ ->
+                val updatedSite = site.copy(
+                    name = dialogBinding.siteName.text.toString(),
+                    address = dialogBinding.siteAddress.text.toString(),
+                    contactName = dialogBinding.contactName.text.toString(),
+                    contactPhone = dialogBinding.contactPhone.text.toString(),
+                    notes = dialogBinding.notes.text.toString()
+                )
+                viewModel.updateSite(updatedSite)
+                dialog.dismiss()
+            }
+            .setNegativeButton("Annuler") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun showDeleteConfirmation(site: Site) {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Supprimer le site")
+            .setMessage("Voulez-vous vraiment supprimer le site ${site.name} ?")
+            .setPositiveButton("Supprimer") { dialog, _ ->
+                viewModel.deleteSite(site)
+                dialog.dismiss()
+            }
+            .setNegativeButton("Annuler") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
