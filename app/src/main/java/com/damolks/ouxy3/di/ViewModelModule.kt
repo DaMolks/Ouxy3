8d7ffc1c86d527844b7ef4c1e3d4eb9905ab5537
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-18 21:44:17 +0100

    feat: Ajout des ViewModels dans le module Koin

--- app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
+++ app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
@@ -1,20 +1,11 @@
 package com.damolks.ouxy3.di
 
-import com.damolks.ouxy3.ui.main.home.HomeViewModel
-import com.damolks.ouxy3.ui.main.sites.SitesViewModel
-import com.damolks.ouxy3.ui.onboarding.signature.SignatureViewModel
-import com.damolks.ouxy3.ui.onboarding.sites.SitesSetupViewModel
-import com.damolks.ouxy3.ui.onboarding.technician.TechnicianProfileViewModel
+import com.damolks.ouxy3.ui.main.marketplace.MarketplaceViewModel
+import com.damolks.ouxy3.ui.main.settings.SettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    // Onboarding ViewModels
-    viewModel { TechnicianProfileViewModel(get()) }
-    viewModel { SignatureViewModel(get()) }
-    viewModel { SitesSetupViewModel(get()) }
-
-    // Main ViewModels
-    viewModel { HomeViewModel(get(), get()) }
-    viewModel { SitesViewModel(get()) }
+    viewModel { MarketplaceViewModel(get()) }
+    viewModel { SettingsViewModel(get()) }
 }
