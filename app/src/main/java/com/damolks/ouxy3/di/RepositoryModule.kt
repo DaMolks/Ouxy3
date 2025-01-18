02904f52580c4c1f7cb127f4deda99d496728895
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-18 21:50:10 +0100

    feat: Ajout du repository technicien dans le module

--- app/src/main/java/com/damolks/ouxy3/di/RepositoryModule.kt
+++ app/src/main/java/com/damolks/ouxy3/di/RepositoryModule.kt
@@ -3,10 +3,13 @@ package com.damolks.ouxy3.di
 import com.damolks.ouxy3.data.repository.MarketplaceRepository
 import com.damolks.ouxy3.data.repository.UserPreferencesRepository
 import com.damolks.ouxy3.data.repository.UserPreferencesRepositoryImpl
+import com.damolks.ouxy3.data.repository.TechnicianRepository
+import com.damolks.ouxy3.data.repository.TechnicianRepositoryImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single { MarketplaceRepository(get()) }
     single<UserPreferencesRepository> { UserPreferencesRepositoryImpl(androidContext()) }
+    single<TechnicianRepository> { TechnicianRepositoryImpl(get()) }
 }
