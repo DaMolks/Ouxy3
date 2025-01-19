a17d1a9d928726ab303ca4a78cc374beff246614
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-19 17:36:10 +0100

    fix: Ajout de la fonction de conversion TechnicianFormState -> Technician

--- app/src/main/java/com/damolks/ouxy3/ui/onboarding/technician/TechnicianFormState.kt
+++ app/src/main/java/com/damolks/ouxy3/ui/onboarding/technician/TechnicianFormState.kt
@@ -1,9 +1,19 @@
 package com.damolks.ouxy3.ui.onboarding.technician
 
+import com.damolks.ouxy3.data.repository.Technician
+
 data class TechnicianFormState(
     val firstName: String,
     val lastName: String,
     val matricule: String,
     val sector: String,
     val teamLeader: String
+)
+
+fun TechnicianFormState.toTechnician() = Technician(
+    firstName = firstName,
+    lastName = lastName,
+    matricule = matricule,
+    sector = sector,
+    teamLeader = teamLeader
 )
