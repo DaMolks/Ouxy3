909d692184cd05f87cce263131b478d24c1950a4
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-19 17:34:50 +0100

    fix: Ajout de TechnicianProfileViewModel dans viewModelModule

--- app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
+++ app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
@@ -7,7 +7,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
+    viewModel { TechnicianProfileViewModel(get()) }
     viewModel { MarketplaceViewModel(get()) }
     viewModel { SettingsViewModel(get()) }
-    viewModel { TechnicianProfileViewModel(get()) }
 }
