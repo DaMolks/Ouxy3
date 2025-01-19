0856fc31f04f5a8076417d69e84ce527048d59b7
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-19 17:40:41 +0100

    feat: Ajout de getAllTechnicians() dans TechnicianRepository

--- app/src/main/java/com/damolks/ouxy3/data/repository/TechnicianRepository.kt
+++ app/src/main/java/com/damolks/ouxy3/data/repository/TechnicianRepository.kt
@@ -1,9 +1,10 @@
 package com.damolks.ouxy3.data.repository
 
-import com.damolks.ouxy3.ui.onboarding.technician.TechnicianFormState
+import kotlinx.coroutines.flow.Flow
 
 interface TechnicianRepository {
     suspend fun saveTechnician(technician: Technician)
+    fun getAllTechnicians(): Flow<List<Technician>>
 }
 
 data class Technician(
