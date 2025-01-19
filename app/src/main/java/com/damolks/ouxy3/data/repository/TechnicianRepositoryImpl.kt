89a99509692eee5e0ffcbc713b508777a218b706
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-19 17:41:09 +0100

    feat: Ajout de l'implémentation de getAllTechnicians

--- app/src/main/java/com/damolks/ouxy3/data/repository/TechnicianRepositoryImpl.kt
+++ app/src/main/java/com/damolks/ouxy3/data/repository/TechnicianRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.damolks.ouxy3.data.repository
 
 import com.damolks.ouxy3.data.db.TechnicianDao
+import com.damolks.ouxy3.data.model.toDomain
 import com.damolks.ouxy3.data.model.toEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class TechnicianRepositoryImpl(
     private val technicianDao: TechnicianDao
@@ -9,5 +12,11 @@ class TechnicianRepositoryImpl(
 
     override suspend fun saveTechnician(technician: Technician) {
         technicianDao.insertTechnician(technician.toEntity())
+    }
+
+    override fun getAllTechnicians(): Flow<List<Technician>> {
+        return technicianDao.getAllTechnicians().map { entities ->
+            entities.map { it.toDomain() }
+        }
     }
 }
