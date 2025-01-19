36b4a18b1d2d85cac98b9f8192db2a0051630f01
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-19 17:44:50 +0100

    fix: Ajout du SitesSetupViewModel dans le module Koin

--- app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
+++ app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
@@ -3,13 +3,15 @@ package com.damolks.ouxy3.di
 import com.damolks.ouxy3.ui.main.marketplace.MarketplaceViewModel
 import com.damolks.ouxy3.ui.main.settings.SettingsViewModel
 import com.damolks.ouxy3.ui.onboarding.signature.SignatureViewModel
+import com.damolks.ouxy3.ui.onboarding.sites.SitesSetupViewModel
 import com.damolks.ouxy3.ui.onboarding.technician.TechnicianProfileViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { TechnicianProfileViewModel(get()) }
+    viewModel { SignatureViewModel(get()) }
+    viewModel { SitesSetupViewModel(get()) }
     viewModel { MarketplaceViewModel(get()) }
     viewModel { SettingsViewModel(get()) }
-    viewModel { SignatureViewModel(get()) }
 }
