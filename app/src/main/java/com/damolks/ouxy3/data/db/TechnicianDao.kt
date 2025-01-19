ea577285c2f24a07b43b33b4c1f34dd24b3ea366
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-19 17:40:49 +0100

    feat: Ajout de getAllTechnicians() dans TechnicianDao

--- app/src/main/java/com/damolks/ouxy3/data/db/TechnicianDao.kt
+++ app/src/main/java/com/damolks/ouxy3/data/db/TechnicianDao.kt
@@ -3,10 +3,15 @@ package com.damolks.ouxy3.data.db
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.damolks.ouxy3.data.model.TechnicianEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TechnicianDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTechnician(technician: TechnicianEntity)
+
+    @Query("SELECT * FROM technicians")
+    fun getAllTechnicians(): Flow<List<TechnicianEntity>>
 }
