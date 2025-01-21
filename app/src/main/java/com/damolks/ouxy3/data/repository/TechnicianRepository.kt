b3e76e0858658df21ab09cae3b07ea87bb1b49af
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-21 18:12:47 +0100

    Ajout de l'interface TechnicianRepository

--- app/src/main/java/com/damolks/ouxy3/data/repository/TechnicianRepository.kt
+++ app/src/main/java/com/damolks/ouxy3/data/repository/TechnicianRepository.kt
@@ -1,16 +1,9 @@
 package com.damolks.ouxy3.data.repository
 
+import com.damolks.ouxy3.data.model.Technician
 import kotlinx.coroutines.flow.Flow
 
 interface TechnicianRepository {
     suspend fun saveTechnician(technician: Technician)
     fun getAllTechnicians(): Flow<List<Technician>>
 }
-
-data class Technician(
-    val firstName: String,
-    val lastName: String,
-    val matricule: String,
-    val sector: String,
-    val teamLeader: String
-)
