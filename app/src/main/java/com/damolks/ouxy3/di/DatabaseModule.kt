6e392b59f8e3b176b0a057620d4efcf39e1ad9ac
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-21 18:07:42 +0100

    Ajout du module de base de données

--- app/src/main/java/com/damolks/ouxy3/di/DatabaseModule.kt
+++ app/src/main/java/com/damolks/ouxy3/di/DatabaseModule.kt
@@ -1,24 +1,11 @@
 package com.damolks.ouxy3.di
 
-import android.content.Context
-import androidx.room.Room
-import com.damolks.ouxy3.data.db.OuxyDatabase
+import com.damolks.ouxy3.data.local.OuxyDatabase
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val databaseModule = module {
-    single { provideDatabase(androidContext()) }
-    single { provideMarketplaceDao(get()) }
-    single { provideTechnicianDao(get()) }
+    single { OuxyDatabase.getInstance(androidContext()) }
+    single { get<OuxyDatabase>().technicianDao() }
+    single { get<OuxyDatabase>().siteDao() }
 }
-
-private fun provideDatabase(context: Context) =
-    Room.databaseBuilder(
-        context.applicationContext,
-        OuxyDatabase::class.java,
-        "ouxy.db"
-    ).build()
-
-private fun provideMarketplaceDao(database: OuxyDatabase) = database.marketplaceDao()
-
-private fun provideTechnicianDao(database: OuxyDatabase) = database.technicianDao()
