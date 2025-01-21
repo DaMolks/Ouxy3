ed8845dc16418690f06c59acc638f4993d01235b
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-21 18:05:49 +0100

    Ajout de l'interface SiteRepository

--- app/src/main/java/com/damolks/ouxy3/data/repository/SiteRepository.kt
+++ app/src/main/java/com/damolks/ouxy3/data/repository/SiteRepository.kt
@@ -1,23 +1,12 @@
 package com.damolks.ouxy3.data.repository
 
-import com.damolks.ouxy3.data.database.dao.SiteDao
 import com.damolks.ouxy3.data.model.Site
 import kotlinx.coroutines.flow.Flow
-import java.time.LocalDateTime
 
-class SiteRepository(private val siteDao: SiteDao) {
-
-    fun getAllSites(): Flow<List<Site>> = siteDao.getAllSites()
-
-    suspend fun getSiteById(id: Long): Site? = siteDao.getSiteById(id)
-
-    suspend fun insertSite(site: Site): Long = siteDao.insertSite(site)
-
-    suspend fun updateSite(site: Site) {
-        siteDao.updateSite(
-            site.copy(updatedAt = LocalDateTime.now())
-        )
-    }
-
-    suspend fun deleteSite(site: Site) = siteDao.deleteSite(site)
+interface SiteRepository {
+    fun getAllSites(): Flow<List<Site>>
+    suspend fun getSiteById(id: Long): Site?
+    suspend fun insertSite(site: Site): Long
+    suspend fun updateSite(site: Site)
+    suspend fun deleteSite(site: Site)
 }
