083c2b31d01f21df9191a69d57eec22d038bbf8b
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-21 18:23:22 +0100

    Ajout de HomeViewModel dans les injections Koin

--- app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
+++ app/src/main/java/com/damolks/ouxy3/di/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.damolks.ouxy3.di
 
+import com.damolks.ouxy3.ui.main.home.HomeViewModel
 import com.damolks.ouxy3.ui.main.marketplace.MarketplaceViewModel
 import com.damolks.ouxy3.ui.main.settings.SettingsViewModel
 import com.damolks.ouxy3.ui.onboarding.signature.SignatureViewModel
@@ -9,9 +10,13 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
+    // Onboarding ViewModels
     viewModel { TechnicianProfileViewModel(get()) }
     viewModel { SignatureViewModel(get()) }
     viewModel { SitesSetupViewModel(get()) }
+
+    // Main ViewModels
+    viewModel { HomeViewModel(get(), get()) }
     viewModel { MarketplaceViewModel(get()) }
     viewModel { SettingsViewModel(get()) }
 }
